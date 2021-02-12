b6a24d45463b9248185473a85d18c054924f646e
EnderBoy2000 <Jakob W@C026754.fritz.box>
2021-02-12 12:06:13 +0100

    CREEPER AWW MAN

---
+++ src/main/java/net/enderstudios/instanttnt/command/CREEPERAWWMANCommand.java
@@ -0,0 +1,67 @@
+
+package net.enderstudios.instanttnt.command;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.common.util.FakePlayerFactory;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.Commands;
+import net.minecraft.command.CommandSource;
+
+import net.enderstudios.instanttnt.procedures.CREEPERAWWMANCommandExecutedProcedure;
+import net.enderstudios.instanttnt.TntModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+@TntModElements.ModElement.Tag
+public class CREEPERAWWMANCommand extends TntModElements.ModElement {
+	public CREEPERAWWMANCommand(TntModElements instance) {
+		super(instance, 7);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public void registerCommands(RegisterCommandsEvent event) {
+		event.getDispatcher().register(customCommand());
+	}
+
+	private LiteralArgumentBuilder<CommandSource> customCommand() {
+		return LiteralArgumentBuilder.<CommandSource>literal("CREEPER_AWW_MAN").requires(s -> s.hasPermissionLevel(1))
+				.then(Commands.argument("arguments", StringArgumentType.greedyString()).executes(this::execute)).executes(this::execute);
+	}
+
+	private int execute(CommandContext<CommandSource> ctx) {
+		ServerWorld world = ctx.getSource().getWorld();
+		double x = ctx.getSource().getPos().getX();
+		double y = ctx.getSource().getPos().getY();
+		double z = ctx.getSource().getPos().getZ();
+		Entity entity = ctx.getSource().getEntity();
+		if (entity == null)
+			entity = FakePlayerFactory.getMinecraft(world);
+		HashMap<String, String> cmdparams = new HashMap<>();
+		int[] index = {-1};
+		Arrays.stream(ctx.getInput().split("\\s+")).forEach(param -> {
+			if (index[0] >= 0)
+				cmdparams.put(Integer.toString(index[0]), param);
+			index[0]++;
+		});
+		{
+			Map<String, Object> $_dependencies = new HashMap<>();
+			$_dependencies.put("x", x);
+			$_dependencies.put("y", y);
+			$_dependencies.put("z", z);
+			$_dependencies.put("world", world);
+			CREEPERAWWMANCommandExecutedProcedure.executeProcedure($_dependencies);
+		}
+		return 0;
+	}
+}
